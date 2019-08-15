18e50fc9a25a5843975f482d810134fda54a7a16
Ricky McConnell <mr.mcconnell12@yahoo.com>
2019-08-15 14:49:34 -0500

    Created models for project

---
+++ Bangazon-Workforce-Management/Models/Computer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bangazon_Workforce_Management.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+
+        public string Make { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        [Required]
+        public DateTime PurchaseDate { get; set; }
+
+        public DateTime? DecomissionDate { get; set; }
+    }
+}
---
+++ Bangazon-Workforce-Management/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon_Workforce_Management.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public Department Department { get; set; }
+
+        public bool isSupervisor { get; set; }
+    }
+}
